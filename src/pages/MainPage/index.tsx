42c6223f47ef57f5f63605ca5ddadf7697843ef3
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-01-18 16:54:36 +0000

    [FIX] MainPage refactored

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,42 +1,22 @@
 import React, { useState } from 'react';
-import { TPhrase } from '../../types/types';
 import {
-  getData, changePhrase, startGame, startOver,
+  getData, startGame,
 } from '../../store/slices/phrasesSlice';
-import Answer from '../../components/Answer';
-import Question from '../../components/Question';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { useAppDispatch } from '../../hooks/hooks';
 import getNumber from '../../utils/getNumber';
 import { qtyOfPhrases } from '../../utils/constants';
 import styles from './MainPage.module.sass';
 import Button from '../../components/UI/Button/Button';
-import Heart from '../../components/Heart';
-import Smile from '../../components/Smile';
-import GameOver from '../../components/GameOver';
-import Loader from '../../components/Loader';
+import Game from './components/Game';
 
 export default function MainPage() {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const dispatch = useAppDispatch();
-  const {
-    list, phrase, isAnswered, counter, lives, gameOver,
-  } = useAppSelector((state) => state.phrases);
 
-  const getNumberAndData = () => {
+  const handleStart = () => {
     dispatch(startGame(getNumber(qtyOfPhrases)));
     dispatch(getData());
-  };
-  const handleStart = () => {
-    getNumberAndData();
     setIsGameStarted(true);
-  };
-  const handleQuestionChange = () => {
-    dispatch(changePhrase());
-    getNumberAndData();
-  };
-  const handleGameOver = () => {
-    dispatch(startOver());
-    getNumberAndData();
   };
 
   return (
@@ -54,74 +34,7 @@ export default function MainPage() {
         )
         : (
           <section className={styles.gameWindow}>
-            <div className={styles.gameWindow__interface}>
-              { phrase
-                ? (
-                  <div className={styles.gameWindow__phrases}>
-
-                    <div className={styles.gameWindow__phrase}>
-                      { !gameOver
-                        ? <Question definition={phrase.definition} />
-                        : <GameOver /> }
-                    </div>
-
-                    <div className={styles.gameWindow__answers}>
-                      {list.map((question: TPhrase) => (
-                        <Answer
-                          word={question.word}
-                          key={question.defid}
-                          id={question.defid}
-                        />
-                      ))}
-                    </div>
-
-                    {gameOver && (
-                    <div className={styles.gameWindow__nextButton}>
-                      <Button
-                        size="large"
-                        variant="primary"
-                        id="button-start-again"
-                        type="button"
-                        value="PLAY AGAIN"
-                        onClick={() => handleGameOver()}
-                      />
-                    </div>
-                    )}
-
-                    {isAnswered && !gameOver
-                    && (
-                    <div className={styles.gameWindow__nextButton}>
-                      <Button
-                        size="large"
-                        variant="primary"
-                        id="button-next"
-                        type="button"
-                        value="NEXT"
-                        onClick={() => handleQuestionChange()}
-                      />
-                    </div>
-                    )}
-
-                  </div>
-                )
-                : (
-                  <div className={styles.gameWindow__loading}>
-                    <Loader />
-                  </div>
-                )}
-
-              <div className={styles.gameWindow__stats}>
-                <div className={styles.gameWindow__points}>{`Points: ${counter}`}</div>
-                <div className={styles.gameWindow__lives}><Smile /></div>
-                <div className={styles.gameWindow__lives}>
-                  {
-                    lives.map((heart) => <Heart key={heart.id} />)
-                }
-                </div>
-              </div>
-
-            </div>
-
+            <Game />
           </section>
         )}
     </div>
